16df6f040e72ddb78dfcc957408ed0d3bf0f13e9
Petru Rares Sincraian <psincraian@outlook.com>
2019-05-03 17:35:18 +0200

    feat: add ReactGA

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './api/config';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import Project from './container/Project';
 import { defaultTheme } from './shared/theme';
 import Index from './container/Index';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
 const store = ConfigureStore();
 
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-115993635-1');
+}
+
+const browserHistory = createBrowserHistory();
+browserHistory.listen(location => {
+  ReactGA.pageview(location.pathname + location.search);
+});
+
 class App extends Component {
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname);
+  }
+
   render() {
     const ProjectContainer = ({ match }) => {
       return <Project projectId={match.params.projectId} />;
@@ -17,7 +32,7 @@ class App extends Component {
 
     return (
       <Provider store={store}>
-        <Router>
+        <Router history={browserHistory}>
           <MuiThemeProvider theme={defaultTheme}>
             <Switch>
               <Route exact path="/" component={Index} />
