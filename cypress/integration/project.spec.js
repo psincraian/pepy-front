bde3120de393d4c2dc34b85f113a664a68868005
Petru Rares Sincraian <psincraian@outlook.com>
2019-11-30 20:55:03 +0100

    feat: add 404 page (#63)

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -1,20 +1,24 @@
 /// <reference types="Cypress" />
 
 context('Actions', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3000/project/requests')
-    })
-
     it('Show summary', () => {
+        cy.visit('http://localhost:3000/project/requests')
         cy.get('[data-cy=summary]').contains("Total downloads");
     })
 
     it('Show downloads', () => {
+      cy.visit('http://localhost:3000/project/requests')
       cy.get("[data-cy=downloads]").get("tr").should('have.length', 8)
     })
 
     it('Show last 30 days downloads', () => {
+      cy.visit('http://localhost:3000/project/requests')
       cy.get("[data-cy=downloads] button").click();
       cy.get("[data-cy=downloads]").get("tr").should('have.length', 31)
+    })
+
+    it ('Show error when project not found', () => {
+      cy.visit('http://localhost:3000/project/asoidj29383rjf4uncpw9cumem')
+      cy.contains('Not found')
     })
 })
---
+++ src/components/Page404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+function Page404(props) {
+  return (
+    <>
+      <Typography align="center" variant="h2">
+        404 - Not found
+      </Typography>
+    </>
+  );
+}
+
+export default Page404;
