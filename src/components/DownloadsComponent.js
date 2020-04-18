6f38e3c67a47e0aa8ef06d6deffbc62b8030377e
Petru Rares Sincraian <psincraian@outlook.com>
2020-04-18 17:31:35 +0200

    feat: improve performance of versions search box (#137)

--- src/components/DownloadsComponent.js
+++ src/components/DownloadsComponent.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
-import { Card, CardContent, CardHeader } from '@material-ui/core';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import Chip from '@material-ui/core/Chip';
+import React, {Component} from 'react';
+import {Card, CardContent, CardHeader} from '@material-ui/core';
 import DownloadsChart from './DownloadsChart';
 import DownloadsTable from './DownloadsTable';
-import TextField from '@material-ui/core/TextField';
 import withStyles from '@material-ui/core/styles/withStyles';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import VersionSearchBox from "./VersionSearchBox";
 
 const styles = (theme) => ({
   downloadsTable: {
@@ -15,28 +11,13 @@ const styles = (theme) => ({
   },
 });
 
-function formatDownloads(downloads) {
-  var precision = 1;
-  if (downloads % 10 === 0) {
-    precision = 0;
-  }
-  if (downloads < 1000) {
-    return downloads;
-  } else if (downloads < 1000000) {
-    return (downloads / 1000).toFixed(precision) + 'K';
-  } else if (downloads < 1000000000) {
-    return (downloads / 1000000).toFixed(precision) + 'M';
-  }
-
-  return (downloads / 1000000000).toFixed(precision) + 'G';
-}
-
 class DownloadsComponent extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       selectedVersions: this.defaultSelectedVersions(),
+      versions: this.props.data.versions.slice().reverse()
     };
   }
 
@@ -48,7 +29,7 @@ class DownloadsComponent extends Component {
   retrieveDownloads(downloads, selectedVersions) {
     var data = [];
     Object.keys(downloads).forEach((date) => {
-      var row = { date: date };
+      var row = {date: date};
       row['total'] = Object.values(downloads[date]).reduce(
         (carry, x) => carry + x
       );
@@ -67,21 +48,11 @@ class DownloadsComponent extends Component {
   }
 
   updateSelectedVersions = (event, value, reason) => {
-    this.setState({ selectedVersions: value });
+    this.setState({selectedVersions: value});
   };
 
-  retrieveVersionDownloads(version) {
-    let total = 0;
-    for (const d of Object.keys(this.props.data.downloads)) {
-      if (version in this.props.data.downloads[d]) {
-        total += this.props.data.downloads[d][version];
-      }
-    }
-    return total;
-  }
-
   render() {
-    const { classes } = this.props;
+    const {classes} = this.props;
     const downloads = this.retrieveDownloads(
       this.props.data.downloads,
       this.state.selectedVersions
@@ -89,46 +60,11 @@ class DownloadsComponent extends Component {
 
     return (
       <Card data-cy="downloads">
-        <CardHeader title="Downloads" />
+        <CardHeader title="Downloads"/>
         <CardContent>
           <>
-            <Autocomplete
-              multiple
-              options={this.props.data.versions.slice().reverse()}
-              renderTags={(value, getTagProps) =>
-                value.map((option, index) => (
-                  <Chip label={option} {...getTagProps({ index })} />
-                ))
-              }
-              renderOption={(option, { selected }) => (
-                <>
-                  <Box
-                    width={16}
-                    height={16}
-                    borderRadius={2}
-                    marginRight={2}
-                    bgcolor={this.retrieveColor(
-                      this.retrieveVersionDownloads(option)
-                    )}
-                  />
-                  <Typography>{option}</Typography>
-                  <Box mx={1}><Typography color="textSecondary">-</Typography></Box>
-                  <Typography color="textSecondary">
-                    {formatDownloads(this.retrieveVersionDownloads(option)) + '/month'}
-                  </Typography>
-                </>
-              )}
-              filterSelectedOptions
-              onChange={this.updateSelectedVersions}
-              value={this.state.selectedVersions}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant="outlined"
-                  label="Select versions"
-                />
-              )}
-            />
+            <VersionSearchBox versions={this.state.versions} onChange={this.updateSelectedVersions}
+                              selectedVersions={this.state.selectedVersions} downloads={this.props.data.downloads}/>
             <DownloadsChart
               data={downloads}
               selectedVersions={this.state.selectedVersions}
@@ -143,19 +79,6 @@ class DownloadsComponent extends Component {
         </CardContent>
       </Card>
     );
-  }
-
-  retrieveColor(downloads) {
-    let color = '#FFF';
-    if (downloads < 1000) {
-      color = '#FFF';
-    } else if (downloads < 1000000) {
-      color = '#f95d6a';
-    } else if (downloads < 1000000000) {
-      color = '#2f4b7c';
-    }
-
-    return color;
   }
 }
 
---
+++ src/shared/helpers.js
@@ -0,0 +1,22 @@
+
+export function formatDownloads(downloads, maxPrecision = 2) {
+  let letter = null;
+  if (downloads < 1000) {
+    letter = '';
+  } else if (downloads < 1000000) {
+    downloads = downloads / 1000;
+    letter = 'k';
+  } else if (downloads < 1000000000) {
+    downloads = downloads / 1000000;
+    letter = 'M';
+  } else {
+    downloads = downloads / 1000000000;
+    letter = 'G';
+  }
+
+  if (downloads % 10 === 0 || Number.isInteger(downloads)) {
+    return downloads.toFixed(0) + letter;
+  }
+
+  return downloads.toFixed(maxPrecision) + letter;
+}
