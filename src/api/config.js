cf3bd69cd2159c9fec500b3ec36ef0f664bb11be
Petru Rares Sincraian <hi@petru.tech>
2023-02-26 21:17:03 +0100

    feat: improve newsletter

--- src/api/config.js
+++ src/api/config.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Project } from './project';
+import { Subscribe } from './subscribe';
 
 export const ConfigureStore = () => {
   return createStore(
     combineReducers({
       project: Project,
+      subscribe: Subscribe,
     }),
     applyMiddleware(thunk)
   );
--- src/container/Newsletter.js
+++ src/container/Newsletter.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import SearchAppBar from '../components/SearchAppBar';
-import { Link, Typography, Grid, Button } from '@mui/material';
+import { Link, Typography, Grid, Button, FormControl, InputLabel, Input, FormHelperText } from '@mui/material';
 import { withStyles } from '@mui/styles';
 import Footer from '../components/Footer';
 import Emoji from '../components/Emoji';
+import { LoadingButton } from '@mui/lab';
+import { subscribe } from '../api/subscribe';
+import { connect } from 'react-redux';
+import { FETCHING_STATUS } from '../api/constants';
+import DoneIcon from '@mui/icons-material/DoneOutline';
+
 const styles = (theme) => ({
   layout: {
     width: 'auto',
@@ -33,15 +39,50 @@ const styles = (theme) => ({
     bottom: 0,
     width: '100%',
   },
+  subscribeSection: {
+    marginTop: theme.spacing(8),
+  },
   section: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(8),
     [theme.breakpoints.up('sm')]: {
-      marginTop: theme.spacing(8),
+      marginTop: theme.spacing(12),
     },
   },
 });
 
+const mapStateToProps = state => {
+  return {
+    subscribe: state.subscribe,
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  sendSubscribe: (email, project) => {
+    dispatch(subscribe(email, project));
+  },
+});
+
 class Newsletter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      project: '',
+      errors: {},
+    };
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  handleSubmit = event => {
+    this.props.sendSubscribe(this.state.email, this.state.project);
+  };
+
   render() {
     const { classes } = this.props;
 
@@ -51,35 +92,34 @@ class Newsletter extends Component {
         <Grid container justifyContent="center" className={classes.layout}>
           <Grid item xs={12}>
             <Typography variant="h2">
-              Our <u>PRO</u> Newsletter
+              Monthly report
             </Typography>
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="h5">
-              Get more useful data directly to your inbox. You will have two
-              months of data, how your project ranks, and comparison with
-              previous month <Emoji symbol="📊" />
-              <p>
-                Also, you will help mantaining this website live!{' '}
-                <Emoji symbol="🤗" />
-              </p>
+            <Typography variant="h6">
+              Get a useful downloads report directly to your inbox.<Emoji symbol="📊" /> This is the first iteration and we will listen to your feedback
+              more carefully.
             </Typography>
           </Grid>
-          <Grid item align="center" xs={12}>
-            <Link
-              aria-label="Support us"
-              color="textSecondary"
-              component="a"
-              target="_blank"
-              href="https://www.buymeacoffee.com/pepy"
-            >
-              <Button variant="contained" size="large" color="primary">
-                Subscribe now
-              </Button>
-            </Link>
+          <Grid item align="center" id="subscribe_action" className={classes.subscribeSection} xs={12}>
+            <form>
+              <FormControl>
+                <InputLabel required htmlFor="email">Email address</InputLabel>
+                <Input id="email" aria-describedby="email-helper" onChange={this.handleChange('email')} value={this.state.email} />
+                <FormHelperText id="email-helper">We'll never share your email.</FormHelperText>
+              </FormControl>
+              <FormControl>
+                <InputLabel htmlFor="project">Project</InputLabel>
+                <Input required id="project" aria-describedby="project-helper" onChange={this.handleChange('project')} value={this.state.project} />
+                <FormHelperText id="project-helper">The project you are interested in</FormHelperText>
+              </FormControl>
+              <LoadingButton onClick={(e) => this.handleSubmit(e)} endIcon={this.props.subscribe.status === FETCHING_STATUS.fetched ? <DoneIcon/> : null} loading={this.props.subscribe.status === FETCHING_STATUS.fetching} type="submit" variant="contained" size="medium" color="primary">
+                Subscribe
+              </LoadingButton>
+            </form>
           </Grid>
           <Grid item xs={12} md={9}>
-            <Grid container className={classes.section}>
+            <Grid container alignItems="center" className={classes.section}>
               <Grid item xs={12} sm={6}>
                 <Typography variant="h6">Compare to previous month</Typography>
                 <Typography>
@@ -97,15 +137,15 @@ class Newsletter extends Component {
             </Grid>
           </Grid>
           <Grid item xs={12} md={9}>
-            <Grid container className={classes.section}>
-              <Grid item className={classes.imgContainer} xs={12} sm={6}>
+            <Grid container alignItems="center" className={classes.section}>
+              <Grid item className={classes.imgContainer} xs={12} sm={6} order={{ xs: 2, sm: 1 }}>
                 <img
                   className={classes.img}
                   alt="Example of advanced stats"
                   src="/newsletter/advanced_stats.png"
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={6} order={{ xs: 1, sm: 2 }}>
                 <Typography variant="h6">Advanced stats</Typography>
                 <Typography>
                   Check how your project compares to other PyPI projects, what
@@ -115,7 +155,7 @@ class Newsletter extends Component {
             </Grid>
           </Grid>
           <Grid item xs={12} md={9}>
-            <Grid container className={classes.section}>
+            <Grid container alignItems="center" className={classes.section}>
               <Grid item xs={12} sm={6}>
                 <Typography variant="h6">More charts</Typography>
                 <Typography>
@@ -133,13 +173,10 @@ class Newsletter extends Component {
               </Grid>
             </Grid>
           </Grid>
-          <Grid item align="center" xs={12}>
+          <Grid item align="center" className={classes.subscribeSection} xs={12}>
             <Link
-              aria-label="Support us"
               color="textSecondary"
-              component="a"
-              target="_blank"
-              href="https://www.buymeacoffee.com/pepy"
+              href="#subscribe_action"
             >
               <Button variant="contained" size="large" color="primary">
                 Subscribe now
@@ -155,4 +192,7 @@ class Newsletter extends Component {
   }
 }
 
-export default withStyles(styles)(Newsletter);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(Newsletter));
