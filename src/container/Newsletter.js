c8ee20086c9947a86c5c742ff7cbd28e50f07a6e
Tianning Li <ltianningli@gmail.com>
2023-02-27 19:22:02 -0800

    Update news letter

--- src/container/Newsletter.js
+++ src/container/Newsletter.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import SearchAppBar from "../components/SearchAppBar";
 import {
   Link,
@@ -71,179 +71,167 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
-class Newsletter extends Component {
-  constructor(props) {
-    super(props);
+const Newsletter = ({ sendSubscribe, classes, subscribe }) => {
+  const [newsletterState, setNewsletterState] = useState({
+    email: "",
+    project: "",
+    errors: {},
+  });
 
-    this.state = {
-      email: "",
-      project: "",
-      errors: {},
-    };
-  }
-
-  handleChange = (name) => (event) => {
-    this.setState({
-      [name]: event.target.value,
+  const handleChange = (e) => {
+    setNewsletterState({
+      ...newsletterState,
+      [e.target.name]: e.target.value,
     });
   };
 
-  handleSubmit = (event) => {
-    this.props.sendSubscribe(this.state.email, this.state.project);
+  const handleSubmit = (event) => {
+    sendSubscribe(newsletterState.email, newsletterState.project);
   };
 
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <>
-        <SearchAppBar />
-        <Grid container justifyContent="center" className={classes.layout}>
-          <Grid item xs={12}>
-            <Typography variant="h2">Monthly report</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h6">
-              Get a useful downloads report directly to your inbox.
-              <Emoji symbol="📊" /> This is the first iteration and we will
-              listen to your feedback more carefully.
-            </Typography>
-          </Grid>
-          <Grid
-            item
-            align="center"
-            id="subscribe_action"
-            className={classes.subscribeSection}
-            xs={12}
-          >
-            <form>
-              <FormControl>
-                <InputLabel required htmlFor="email">
-                  Email address
-                </InputLabel>
-                <Input
-                  id="email"
-                  aria-describedby="email-helper"
-                  onChange={this.handleChange("email")}
-                  value={this.state.email}
-                />
-                <FormHelperText id="email-helper">
-                  We'll never share your email.
-                </FormHelperText>
-              </FormControl>
-              <FormControl>
-                <InputLabel htmlFor="project">Project</InputLabel>
-                <Input
-                  required
-                  id="project"
-                  aria-describedby="project-helper"
-                  onChange={this.handleChange("project")}
-                  value={this.state.project}
-                />
-                <FormHelperText id="project-helper">
-                  The project you are interested in
-                </FormHelperText>
-              </FormControl>
-              <LoadingButton
-                onClick={(e) => this.handleSubmit(e)}
-                endIcon={
-                  this.props.subscribe.status === FETCHING_STATUS.fetched ? (
-                    <DoneIcon />
-                  ) : null
-                }
-                loading={
-                  this.props.subscribe.status === FETCHING_STATUS.fetching
-                }
-                type="submit"
-                variant="contained"
-                size="medium"
-                color="primary"
-              >
-                Subscribe
-              </LoadingButton>
-            </form>
-          </Grid>
-          <Grid item xs={12} md={9}>
-            <Grid container alignItems="center" className={classes.section}>
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h6">Compare to previous month</Typography>
-                <Typography>
-                  You will get two months of data, to compare how the project is
-                  performing this month and the previous one.
-                </Typography>
-              </Grid>
-              <Grid item className={classes.imgContainer} xs={12} sm={6}>
-                <img
-                  alt="Example of monthly download stats"
-                  className={classes.img}
-                  src="/newsletter/monthly_downloads.png"
-                />
-              </Grid>
+  return (
+    <>
+      <SearchAppBar />
+      <Grid container justifyContent="center" className={classes.layout}>
+        <Grid item xs={12}>
+          <Typography variant="h2">Monthly report</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">
+            Get a useful downloads report directly to your inbox.
+            <Emoji symbol="📊" /> This is the first iteration and we will listen
+            to your feedback more carefully.
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          align="center"
+          id="subscribe_action"
+          className={classes.subscribeSection}
+          xs={12}
+        >
+          <form>
+            <FormControl>
+              <InputLabel required htmlFor="email">
+                Email address
+              </InputLabel>
+              <Input
+                id="email"
+                name="email"
+                aria-describedby="email-helper"
+                onChange={handleChange}
+                value={newsletterState.email}
+              />
+              <FormHelperText id="email-helper">
+                We'll never share your email.
+              </FormHelperText>
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="project">Project</InputLabel>
+              <Input
+                required
+                id="project"
+                name="project"
+                aria-describedby="project-helper"
+                onChange={handleChange}
+                value={newsletterState.project}
+              />
+              <FormHelperText id="project-helper">
+                The project you are interested in
+              </FormHelperText>
+            </FormControl>
+            <LoadingButton
+              onClick={handleSubmit}
+              endIcon={
+                subscribe.status === FETCHING_STATUS.fetched ? (
+                  <DoneIcon />
+                ) : null
+              }
+              loading={subscribe.status === FETCHING_STATUS.fetching}
+              type="submit"
+              variant="contained"
+              size="medium"
+              color="primary"
+            >
+              Subscribe
+            </LoadingButton>
+          </form>
+        </Grid>
+        <Grid item xs={12} md={9}>
+          <Grid container alignItems="center" className={classes.section}>
+            <Grid item xs={12} sm={6}>
+              <Typography variant="h6">Compare to previous month</Typography>
+              <Typography>
+                You will get two months of data, to compare how the project is
+                performing this month and the previous one.
+              </Typography>
             </Grid>
-          </Grid>
-          <Grid item xs={12} md={9}>
-            <Grid container alignItems="center" className={classes.section}>
-              <Grid
-                item
-                className={classes.imgContainer}
-                xs={12}
-                sm={6}
-                order={{ xs: 2, sm: 1 }}
-              >
-                <img
-                  className={classes.img}
-                  alt="Example of advanced stats"
-                  src="/newsletter/advanced_stats.png"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6} order={{ xs: 1, sm: 2 }}>
-                <Typography variant="h6">Advanced stats</Typography>
-                <Typography>
-                  Check how your project compares to other PyPI projects, what
-                  are the growing versions, and the most popular ones.
-                </Typography>
-              </Grid>
+            <Grid item className={classes.imgContainer} xs={12} sm={6}>
+              <img
+                alt="Example of monthly download stats"
+                className={classes.img}
+                src="/newsletter/monthly_downloads.png"
+              />
             </Grid>
-          </Grid>
-          <Grid item xs={12} md={9}>
-            <Grid container alignItems="center" className={classes.section}>
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h6">More charts</Typography>
-                <Typography>
-                  You will have a deeper view of how your project versions are
-                  distributed and how is the evolution of this versions. The
-                  more you know the better.
-                </Typography>
-              </Grid>
-              <Grid item className={classes.imgContainer} xs={12} sm={6}>
-                <img
-                  className={classes.img}
-                  alt="Example of different graph"
-                  src="/newsletter/downloads_per_version.png"
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid
-            item
-            align="center"
-            className={classes.subscribeSection}
-            xs={12}
-          >
-            <Link color="textSecondary" href="#subscribe_action">
-              <Button variant="contained" size="large" color="primary">
-                Subscribe now
-              </Button>
-            </Link>
           </Grid>
         </Grid>
-        <div className={classes.footer}>
-          <Footer />
-        </div>
-      </>
-    );
-  }
-}
+        <Grid item xs={12} md={9}>
+          <Grid container alignItems="center" className={classes.section}>
+            <Grid
+              item
+              className={classes.imgContainer}
+              xs={12}
+              sm={6}
+              order={{ xs: 2, sm: 1 }}
+            >
+              <img
+                className={classes.img}
+                alt="Example of advanced stats"
+                src="/newsletter/advanced_stats.png"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} order={{ xs: 1, sm: 2 }}>
+              <Typography variant="h6">Advanced stats</Typography>
+              <Typography>
+                Check how your project compares to other PyPI projects, what are
+                the growing versions, and the most popular ones.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={9}>
+          <Grid container alignItems="center" className={classes.section}>
+            <Grid item xs={12} sm={6}>
+              <Typography variant="h6">More charts</Typography>
+              <Typography>
+                You will have a deeper view of how your project versions are
+                distributed and how is the evolution of this versions. The more
+                you know the better.
+              </Typography>
+            </Grid>
+            <Grid item className={classes.imgContainer} xs={12} sm={6}>
+              <img
+                className={classes.img}
+                alt="Example of different graph"
+                src="/newsletter/downloads_per_version.png"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item align="center" className={classes.subscribeSection} xs={12}>
+          <Link color="textSecondary" href="#subscribe_action">
+            <Button variant="contained" size="large" color="primary">
+              Subscribe now
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+      <div className={classes.footer}>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 export default connect(
   mapStateToProps,
