30f175ec0d35a963fea89358d41067762600a2e5
Petru Rares Sincraian <psincraian@outlook.com>
2023-07-08 17:24:34 +0200

    feat: remove evrything

--- src/api/constants.js
+++
@@ -1,9 +0,0 @@
-export const FETCHING_STATUS = {
-  noData: 0,
-  fetching: 1,
-  fetched: 2,
-  error: 3,
-};
-
-export const BASE_URL = 'https://analytics.pepy.tech';
-export const ANALYTICS_BASE_URL = 'https://analytics.pepy.tech';
