c768cd4e71dfce9fa269dbf9f9d8778d42ab2d6e
Petru Rares Sincraian <petrurs@amazon.es>
2023-08-17 09:15:44 +0200

    feat: add cypress back

---
+++ cypress/e2e/search_project.cy.ts
@@ -0,0 +1,10 @@
+describe('Should show project page', () => {
+  it('Should navigate to the project page', () => {
+    cy.visit('/')
+
+    cy.get('.MuiInputBase-input').type('requests')
+    cy.get('[data-cy="search-button"]').click()
+
+    cy.contains('https://pypi.org/project/requests')
+  })
+})
