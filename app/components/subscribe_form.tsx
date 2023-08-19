64c182c6ea2973722284134ecb9b3770a835a787
Petru Rares Sincraian <psincraian@outlook.com>
2023-08-19 19:48:46 +0200

    feat: add newsletter page back

---
+++ app/components/subscribe_form.tsx
@@ -0,0 +1,126 @@
+'use client';
+import React, {useState} from "react";
+import {FormControl, FormHelperText, Grid, Input, InputLabel} from "@mui/material";
+import {LoadingButton} from "@mui/lab";
+import {DoneOutline, ErrorOutline} from "@mui/icons-material";
+
+const VALID_EMAIL_REGEX = /^(.+)@(.+)\.(.+)$/;
+
+enum SubmissionStatus {
+    NO_FETCHING,
+    FETCHING,
+    COMPLETED_OK,
+    COMPLETED_FAILURE
+}
+
+export const SubscribeForm = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        project: '',
+        emailErrors: '',
+    });
+
+    const [submissionStatus, setSubmissionStatus] = useState({
+        status: SubmissionStatus.NO_FETCHING,
+    })
+
+    const handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [name]: event.target.value,
+            emailErrors: name !== 'email' || event.target.value.match(VALID_EMAIL_REGEX)
+                ? ''
+                : 'Invalid email',
+        });
+    };
+
+    const handleSubmit = () => {
+        if (formData.email.match(VALID_EMAIL_REGEX)) {
+            setSubmissionStatus({status: SubmissionStatus.FETCHING})
+            fetch(`https://analytics.pepy.tech/subscriptions`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: formData.email,
+                    project: formData.project,
+                }),
+            }).then(r => {
+                if (r.ok) {
+                    setSubmissionStatus({status: SubmissionStatus.COMPLETED_OK})
+                } else {
+                    setSubmissionStatus({status: SubmissionStatus.COMPLETED_FAILURE});
+                }
+            })
+        } else {
+            setFormData(prevState => ({
+                ...prevState,
+                emailErrors: 'Invalid email',
+            }));
+        }
+    };
+
+    let endIcon = null;
+    if (submissionStatus.status === SubmissionStatus.COMPLETED_FAILURE) {
+        endIcon = <ErrorOutline/>;
+    } else if (submissionStatus.status === SubmissionStatus.COMPLETED_OK) {
+        endIcon = <DoneOutline/>;
+    }
+
+    return (
+        <Grid
+            container
+            alignItems="center"
+            justifyContent="center"
+            spacing={4}
+        >
+            <Grid item xs={12} sm={4}>
+                <FormControl fullWidth>
+                    <InputLabel required htmlFor="email">
+                        Email address
+                    </InputLabel>
+                    <Input
+                        id="email"
+                        aria-describedby="email-helper"
+                        onChange={handleChange('email')}
+                        error={formData.emailErrors !== ''}
+                        value={formData.email}
+                    />
+                    <FormHelperText id="email-helper">
+                        We&apos;ll never share your email.
+                    </FormHelperText>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <FormControl fullWidth>
+                    <InputLabel htmlFor="project">Project</InputLabel>
+                    <Input
+                        required
+                        id="project"
+                        aria-describedby="project-helper"
+                        onChange={handleChange('project')}
+                        value={formData.project}
+                    />
+                    <FormHelperText id="project-helper">
+                        The project you are interested in
+                    </FormHelperText>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <LoadingButton
+                    fullWidth
+                    onClick={() => handleSubmit()}
+                    endIcon={endIcon}
+                    loading={submissionStatus.status === SubmissionStatus.FETCHING}
+                    type="submit"
+                    variant="contained"
+                    size="medium"
+                    color="primary"
+                >
+                    Subscribe
+                </LoadingButton>
+            </Grid>
+        </Grid>
+    );
+};
--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -1,25 +1,94 @@
+import React from 'react';
+import {
+    Grid, Typography,
+} from '@mui/material';
 import AppBar from "@/app/components/app_bar";
+import {SubscribeForm} from "@/app/components/subscribe_form";
+import Emoji from "@/app/components/emoji";
+import styles from './page.module.css';
 
-export const runtime = 'edge';
-
-
-export default async function Page() {
-
+const Newsletter = () => {
     return (
         <>
             <AppBar/>
-            <main
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "70vh",
-                }}>
-                <div>
-                    <h1>Newsletter</h1>
-                    <p>Soon it will be back :)</p>
-                </div>
+            <main>
+                <Grid container justifyContent="center" rowSpacing={6}>
+                    <Grid item xs={12}>
+                        <Typography variant="h2">Monthly report</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="h6">
+                            Get a useful downloads report directly to your inbox.
+                            <Emoji symbol="📊"/>
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <SubscribeForm/>
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Grid container alignItems="center">
+                            <Grid item xs={12} sm={6}>
+                                <Typography variant="h6">Compare to previous month</Typography>
+                                <Typography>
+                                    You will get two months of data, to compare how the project is
+                                    performing this month and the previous one.
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <img
+                                    className={styles.img}
+                                    alt="Example of monthly download stats"
+                                    src="/newsletter/monthly_downloads.png"
+                                />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Grid container alignItems="center">
+                            <Grid
+                                item
+                                xs={12}
+                                sm={6}
+                                order={{xs: 2, sm: 1}}
+                            >
+                                <img
+                                    className={styles.img}
+                                    alt="Example of advanced stats"
+                                    src="/newsletter/advanced_stats.png"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6} order={{xs: 1, sm: 2}}>
+                                <Typography variant="h6">Advanced stats</Typography>
+                                <Typography>
+                                    Check how your project compares to other PyPI projects, what
+                                    are the growing versions, and the most popular ones.
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Grid container alignItems="center">
+                            <Grid item xs={12} sm={6}>
+                                <Typography variant="h6">More charts</Typography>
+                                <Typography>
+                                    You will have a deeper view of how your project versions are
+                                    distributed and how is the evolution of this versions. The
+                                    more you know the better.
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <img
+                                    className={styles.img}
+                                    alt="Example of different graph"
+                                    src="/newsletter/downloads_per_version.png"
+                                />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </main>
         </>
     );
-}
+};
+
+export default Newsletter;
