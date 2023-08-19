5f88b260d87abae46eae7f185e75f7efb129920e
Petru Rares Sincraian <psincraian@outlook.com>
2023-08-19 18:04:38 +0200

    styles: add padding in table

--- app/components/downloads_table.tsx
+++ app/components/downloads_table.tsx
@@ -21,7 +21,7 @@ const DownloadsTable: React.FC<DownloadsChartProps> = (props) => {
     const downloads = data.reverse();
 
     return (
-        <TableContainer sx={{maxHeight: '500px'}}>
+        <TableContainer sx={{paddingTop: '16px', maxHeight: '500px'}}>
             <Table id="downloads-table" stickyHeader>
                 <TableHead>
                     <TableRow>
