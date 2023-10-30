476c610422feb811830e5f6065780185a8819fd9
Petru Rares Sincraian <psincraian@outlook.com>
2023-10-30 08:18:05 +0100

    feat: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/app/components/footer";
 
 export const metadata = {
   title: "pepy.tech - Python Packages Download Stats",
-  description: "View download stats for more than 490k python packages. Stats are updated daily",
+  description: "View download stats for more than 490k python packages. PyPI Download stats are updated daily",
 };
 
 export const runtime = "edge";
