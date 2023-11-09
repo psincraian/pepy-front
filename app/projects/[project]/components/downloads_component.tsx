654224ddcdccd7a5ed60ddfb79b4e23f82d1a350
Petru Rares Sincraian <psincraian@outlook.com>
2023-11-09 08:48:49 +0100

    wip

--- app/projects/[project]/components/downloads_component.tsx
+++ app/projects/[project]/components/downloads_component.tsx
@@ -11,6 +11,14 @@ import DownloadsTable from "@/app/projects/[project]/components/downloads_table"
 import { DisplayStyleToggle } from "@/app/projects/[project]/components/display_style_toggle";
 import { Grid } from "@mui/material";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { Project } from "@/app/projects/[project]/model";
+import { PEPY_HOST } from "@/app/constants";
+import { notFound } from "next/navigation";
+import { VersionDownloads } from "@/app/projects/[project]/model";
+import { useEffect } from "react";
+import { DownloadsResponse } from "@/app/projects/[project]/helper/compute_downloads";
+import { Range } from "@/app/projects/[project]/model";
+import { RangeToggle } from "@/app/projects/[project]/components/range_toggle";
 
 interface DownloadsChartProps {
   versions: string[];
@@ -29,6 +37,34 @@ const updateSelectedVersions = (
   setSelectedVersions(versions);
 };
 
+async function getData(project: string): Promise<DownloadData> {
+  console.log("Fetching data for", project);
+  const res = await fetch( `/api/v3/pro/projects/${project}/downloads`, {
+    headers: {
+      'X-Api-Key': process.env.PEPY_API_KEY!,
+    },
+    next: { revalidate: 3600 },
+  });
+  if (res.status === 404) {
+    notFound();
+  } else if (res.status !== 200) {
+    throw new Error(`Server error: ${res.status}`);
+  }
+
+  const downloadData: DownloadData = {};
+  let response = await res.json();
+  for (const [date, downloads] of Object.entries(response.downloads)) {
+    const verionDownloads: VersionDownloads = {};
+    for (const [version, count] of Object.entries(downloads!)) {
+      verionDownloads[version] = count;
+    }
+
+    downloadData[date] = verionDownloads;
+  }
+
+  return downloadData;
+}
+
 const DownloadsComponent: React.FC<DownloadsChartProps> = (props) => {
   const router = useRouter();
   const pathname = usePathname();
@@ -37,10 +73,22 @@ const DownloadsComponent: React.FC<DownloadsChartProps> = (props) => {
     ? searchParams.getAll("versions")
     : undefined;
 
+  const [downloads, setDownloads] = useState<DownloadsResponse[]>([])
+
+
+
   const [selectedVersions, setSelectedVersions] = useState(
     userVersions ?? defaultSelectedVersions(props.versions),
   );
-  const [displayStyle, setDisplayStyle] = useState(DisplayStyle.DAILY);
+  const [displayStyle, setDisplayStyle] = useState(DisplayStyle.WEEKLY);
+  const [range, setRange] = useState(Range.FOUR_MONTHS);
+
+  useEffect(() => {
+    getData("requests").then(data => {
+      const response = retrieveDownloads(data, selectedVersions, displayStyle);
+      setDownloads(response)
+    });
+  }, [selectedVersions, displayStyle, range])
 
   const versions = props.versions.map((version) => ({
     title: version,
@@ -50,11 +98,7 @@ const DownloadsComponent: React.FC<DownloadsChartProps> = (props) => {
     title: version,
     value: version,
   }));
-  const downloads = retrieveDownloads(
-    props.data,
-    selectedVersions,
-    displayStyle,
-  );
+
   return (
     <div className={styles.root}>
       <Grid container spacing={2} alignItems={"center"}>
@@ -71,6 +115,12 @@ const DownloadsComponent: React.FC<DownloadsChartProps> = (props) => {
                 versions,
               )
             }
+          />
+        </Grid>
+        <Grid item xs={12} sm="auto">
+          <RangeToggle
+            selected={range}
+            handleChange={setRange}
           />
         </Grid>
         <Grid item xs={12} sm="auto">
---
+++ app/projects/[project]/components/range_toggle.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { DisplayStyle } from "@/app/projects/[project]/model";
+import { useEffect, useState } from "react";
+import { getCurrentUser, User } from "@/app/user/helper/auth";
+import styles from "./display_style_toggle.module.css";
+import LoggedUsersTooltip from "@/app/components/logged_users_tooltip";
+import { Range } from "@/app/projects/[project]/model";
+
+export interface Display_style_toggle {
+  selected: Range;
+  handleChange: (value: Range) => void;
+}
+
+export const RangeToggle = ({
+  selected,
+  handleChange,
+}: Display_style_toggle) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    getCurrentUser().then((user) => setCurrentUser(user));
+  }, []);
+
+  const disabled = currentUser === null;
+  const buttonStyles = disabled
+    ? styles.button + " " + styles.disabled
+    : styles.button;
+  const buttonSelectedStyles = disabled
+    ? styles.selected + " " + styles.button + " " + styles.disabled
+    : styles.selected + " " + styles.button;
+
+  return (
+    <div className={styles.toggleGroup}>
+      <button
+        className={
+          selected === Range.FOUR_MONTHS
+            ? buttonSelectedStyles + " " + styles.first
+            : styles.button + " " + styles.first
+        }
+        onClick={() => handleChange(Range.FOUR_MONTHS)}
+      >
+        4 Months
+      </button>
+      <button
+        disabled={disabled}
+        className={
+          selected === Range.ONE_YEAR
+            ? buttonSelectedStyles + " " + styles.third
+            : buttonStyles + " " + styles.third
+        }
+        onClick={() => handleChange(Range.ONE_YEAR)}
+      >
+        <LoggedUsersTooltip display={disabled}>
+          <span>12 Months</span>
+        </LoggedUsersTooltip>
+      </button>
+    </div>
+  );
+};
