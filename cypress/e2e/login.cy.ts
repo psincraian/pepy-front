b4cd59dbda3c0af8de3cc2574075b1dc8e1a1599
Petru Rares Sincraian <psincraian@outlook.com>
2023-12-22 20:39:14 +0100

    test: add login test (#827)
    * test: add login e2e test
    
    * test: add login e2e test

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,13 @@
+describe("Should login user", () => {
+  it("With valid credentials", () => {
+    cy.visit("/user/login", { failOnStatusCode: false });
+
+    console.log(Cypress.env("PEPY_TEST_USER_EMAIL"))
+    console.log(Cypress.env("CYPRESS_PEPY_TEST_USER_EMAIL"))
+    cy.get("#email").type(Cypress.env("PEPY_TEST_USER_EMAIL"));
+    cy.get("#password").type(Cypress.env('PEPY_TEST_USER_PASSWORD'));
+    cy.get("#login-button").click();
+
+    cy.contains("Hello petrutest");
+  });
+});
