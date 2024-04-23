171243ecc05e20808d7528f3fced2def1fdfa32d
Petru Rares Sincraian <psincraian@outlook.com>
2024-04-23 10:36:37 +0200

    feat: add logging for exceptions

--- middleware.ts
+++ middleware.ts
@@ -6,16 +6,22 @@ export const config = {
 }
 
 export function middleware(request: NextRequest) {
-    const requestHeaders = new Headers(request.headers)
-    const accessToken = request.cookies.getAll().filter(cookie => cookie.name.includes('accessToken'));
-    if (accessToken.length === 1) {
-        requestHeaders.set('Authorization', 'Bearer ' + accessToken[0].value);
+    try {
+        const requestHeaders = new Headers(request.headers)
+        const accessToken = request.cookies.getAll().filter(cookie => cookie.name.includes('accessToken'));
+        if (accessToken.length === 1) {
+            requestHeaders.set('Authorization', 'Bearer ' + accessToken[0].value);
+        }
+        requestHeaders.set('X-Api-Key', process.env.PEPY_API_KEY!);
+
+        return NextResponse.next({
+            request: {
+                // New request headers
+                headers: requestHeaders,
+            },
+        });
+    } catch (error) {
+        console.log(error);
     }
-    requestHeaders.set('X-Api-Key', process.env.PEPY_API_KEY!);
-    return NextResponse.next({
-        request: {
-            // New request headers
-            headers: requestHeaders,
-        },
-    });
+
 }
