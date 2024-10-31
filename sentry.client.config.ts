973ad94d9e2e1bf09e56d03972280219ff57f99e
Petru Rares Sincraian <psincraian@outlook.com>
2024-10-31 06:38:57 +0100

    feat: decrease the sample rate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -11,7 +11,7 @@ Sentry.init({
   integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
