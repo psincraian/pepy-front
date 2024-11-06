1e582847841aa5ca94818efac01133a2adf4f83a
Petru Rares Sincraian <psincraian@outlook.com>
2024-11-06 20:29:07 +0100

    feat: add source maps

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         source: "/project/:project",
         destination: "/projects/:project",
         permanent: true
-      }
+      },
     ];
   },
   async rewrites() {
@@ -17,15 +17,59 @@ const nextConfig = {
       {
         source: "/api/:path*",
         destination: PEPY_HOST + "/api/:path*"
-      }
+      },
     ];
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === "production"
-  }
+  },
 };
 
 module.exports = nextConfig;
+
+// Injected content via Sentry wizard below
+
+const { withSentryConfig } = require("@sentry/nextjs");
+
+module.exports = withSentryConfig(module.exports, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  org: "pepy-5g",
+  project: "javascript-nextjs",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true
+  },
+
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true
+});
 
 // Injected content via Sentry wizard below
 
