b45410d05cfab127544bc76c1afe1dc8eb99aef7
Petru Rares Sincraian <psincraian@outlook.com>
2024-11-07 12:18:50 +0100

    fix: disable tunnel route

--- next.config.js
+++ next.config.js
@@ -54,7 +54,7 @@ module.exports = withSentryConfig(module.exports, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: "/monitoring",
+  tunnelRoute: null,
 
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
